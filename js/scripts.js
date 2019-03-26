57269682beb0a1e633a89de781d8978ed2cad109
“Paul <prowori2@gmail.com>
2019-03-26 10:38:17 +0300

    Add disappearing function to calc

---
+++ js/scripts.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+
+  $("#9").click(function() {
+    $("#9").toggle();
+  });
+
+  $("#8").click(function() {
+    $("#8").toggle();
+  });
+
+  $("#7").click(function() {
+    $("#7").toggle();
+  });
+
+  $("#6").click(function() {
+    $("#6").toggle();
+  });
+
+  $("#5").click(function() {
+    $("#5").toggle();
+  });
+
+  $("#4").click(function() {
+    $("#4").toggle();
+  });
+
+  $("#3").click(function() {
+    $("#3").toggle();
+  });
+
+  $("#2").click(function() {
+    $("#2").toggle();
+  });
+
+  $("#1").click(function() {
+    $("#1").toggle();
+  });
+
+});
+
+
+$(document).ready(function(){
+  $("#plus").click(function() {
+    $("#plus").toggle();
+  });
+
+  $("#minus").click(function() {
+    $("#minus").toggle();
+  });
+
+  $("#stars").click(function() {
+    $("#stars").toggle();
+  });
+
+  $("#zero").click(function() {
+    $("#zero").toggle();
+  });
+
+  $("#slash").click(function() {
+    $("#slash").toggle();
+  });
+
+  $("#modulo").click(function() {
+    $("#modulo").toggle();
+  });
+
+  $("#screen").click(function() {
+    $("#screen").toggle();
+  });
+
+  $("#slashd").click(function() {
+    $("#slashd").toggle();
+  });
+
+  $("#equal").click(function() {
+    $("#equal").toggle();
+  });
+
+  $("#rem").click(function() {
+    $("#rem").toggle();
+  });
+
+});
